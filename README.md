c132b511c538ebe8e593a921d9b35048f4e5a3ca
Neil Smith <s3nt1n3lz18@gmail.com>
2020-10-10 14:52:33 +0100

    Now grabbing ngIfs

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ A Script To Check The Coverage Of Your Angular Templates
 
 This is a simple script to check the code coverage of your html templates by checking for unit tests that contain the Id of the NgIf and NgFor elements.
 
+NPM  https://www.npmjs.com/package/angular-templates-code-coverage
+Github  https://github.com/s3nt1n3lz21/Angular-Templates-Code-Coverage
+
 ## Install
 
 npm install angular-templates-code-coverage --save-dev
--- index.js
+++ index.js
@@ -1,32 +1,44 @@
 #! /usr/bin/env node
-const https = require('https')
-async function loadFileAndPrintToConsole(url) {
-    // var data = require(url);
-    // https.get(url).subscribe(
-    //     (data) => {
-    //         console.log(data)
-    //         return data
-    //     },
-    //     (error) => {
-    //         console.error(error)
-    //         return null
-    //     }
-    // );
-    console.log('Hello world')
-    // var client = new XMLHttpRequest();
-    // client.open('GET', url);
-    // client.onreadystatechange = function() {
-    // alert(client.responseText);
-    // }
-    // client.send();
+var fs = require('fs');
+function loadFile(path) {
+    let dataString;
+    fs.readFile(path, function (err, data) {
+        if (err) {
+            throw err; 
+        }
+        dataString = data.toString();
+        console.log(path,dataString);
+        console.log('data: ', data)
+        let ngIfs = findNgIf(dataString)
+    });
 }
 
 function main(args) {
-    console.log(args)
-    let file = args[0]
-    const data = loadFileAndPrintToConsole('test.html')
-    console.log(data)
+    let arg1 = args[0]
+    const data = loadFile('./test/assets/test.html')
 };
+
+// Grab a list of all the elements with an ngIf
+function findNgIf(file) {
+    const regexToSearchFor = /<[^/<>]*\*ngIf[^/<>]*>/g;
+    const ngIfElements = file.match(regexToSearchFor);
+    console.log(ngIfElements)
+    console.log(ngIfElements[0])
+}
+
+// Check which elements have an id and print file names and line numbers of ngIfs that do not have an id
+function checkIds() {
+
+}
+
+function hasId() {
+
+}
+
+// Grab all unit test titles
+
+
+// Check for 
 
 var args = process.argv.slice(2);
 main(args);
