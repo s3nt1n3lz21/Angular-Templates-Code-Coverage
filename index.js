4db87efb6b9d57dbb7c565c7a4f19cf1db13206d
Neil Smith <s3nt1n3lz18@gmail.com>
2020-10-10 14:15:23 +0100

    Now running file using a terminal command

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
-module.exports = function main(string) {
-    if (typeof string !== "string") throw new TypeError("Tiny wants a string!");
-    return string.replace(/\s/g, "");
+#! /usr/bin/env node
+const https = require('https')
+async function loadFileAndPrintToConsole(url) {
+    // var data = require(url);
+    https.get(url).subscribe(
+        (data) => {
+            console.log(data)
+            return data
+        },
+        (error) => {
+            console.error(error)
+            return null
+        }
+    );
+    // var client = new XMLHttpRequest();
+    // client.open('GET', url);
+    // client.onreadystatechange = function() {
+    // alert(client.responseText);
+    // }
+    // client.send();
+}
+
+function main(args) {
+    console.log(args)
+    let file = args[0]
+    const data = loadFileAndPrintToConsole('test.html')
+    console.log(data)
 };
+
+var args = process.argv.slice(2);
+main(args);
