48cc6b22b462c2589e66bc721eb72f8be4a8ee4d
Neil Smith <s3nt1n3lz18@gmail.com>
2020-12-22 14:17:11 +0000

    Changed folder structure

---
+++ test/ngIf/ngIfShow/test.component.ts
@@ -0,0 +1,14 @@
+import {
+    Component,
+} from '@angular/core';
+
+@Component({
+    selector: 'app-test',
+    templateUrl: './app-test.component.html',
+    styleUrls: ['./app-test.component.scss']
+})
+export class TestComponent {
+
+    constructor() {
+    }
+}
