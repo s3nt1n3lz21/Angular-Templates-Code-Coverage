4e461d4f54888118aeab32cb1d34d1d226189ae0
Neil Smith <s3nt1n3lz18@gmail.com>
2021-01-17 17:21:43 +0000

    Added export

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ var TemplateCoverage = require('./template-coverage');
 var templateCoverage = new TemplateCoverage();
 var args = process.argv.slice(2);
 templateCoverage.main(args);
+
+module.exports = TemplateCoverage;
